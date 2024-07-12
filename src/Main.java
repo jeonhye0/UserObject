4b9ac313b1a4ed5239746a8640c5b94bf83d3a70
oopsw.jeonhye0@gmail.com <oopsw.jeonhye0@gmail.com>
2024-07-12 16:24:54 +0900

    new window update

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("update1");
         System.out.println("new update1");
+
+        System.out.println("new window update");
     }
 }
